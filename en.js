21c48ff8dd400caedff58e38222c70919402199c
Stefano Verna <s.verna@datocms.com>
2020-06-23 12:25:52 +0200

    Sync from Loco

--- en.js
+++ en.js
@@ -2112,7 +2112,7 @@ module.exports = {
   'usageCounter.period.current_month': `This month`,
   'usageCounter.period.last_month': `Last month`,
   'usageCounter.info': `
-    Counters are updated every minute and can help you to better identify the source of the incoming requests. They’re not to be considered 100%
+    Reports are updated every minute and can help you to better identify the source of the incoming requests. They’re not to be considered 100%
     accurate, and serve for qualitative analysis.
   `,
 };
